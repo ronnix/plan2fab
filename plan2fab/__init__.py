dca3d7ac4936b8792b015aba130f70ec9d1c1a9d
Ronan Amicel <ronan.amicel@gmail.com>
2014-12-14 20:28:46 +0100

    Configure a session factory (needed for "flash messages")

--- plan2fab/__init__.py
+++ plan2fab/__init__.py
@@ -1,12 +1,21 @@
 from pyramid.config import Configurator
+from pyramid.session import SignedCookieSessionFactory
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+
     config = Configurator(settings=settings)
+
+    session_factory = SignedCookieSessionFactory(settings['session_secret'])
+    config.set_session_factory(session_factory)
+
     config.add_static_view('static', 'static', cache_max_age=3600)
+
     config.add_route('home', '/')
     config.add_route('create_project', '/create_project')
+
     config.scan()
+
     return config.make_wsgi_app()
